3650689808d52e34c901cdbebdac1ef1ed36e651
Rezasl99 <Rezasoltani78@gmail.com>
2024-08-23 21:24:15 +0330

    0.0.2

--- accounts/models.py
+++ accounts/models.py
@@ -36,17 +36,17 @@ class AccountManager(BaseUserManager):
         return user
 
 class Account(AbstractBaseUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    phonenumber = models.CharField(max_length=25, unique=True)
+    first_name    = models.CharField(max_length=50)
+    last_name     = models.CharField(max_length=50)
+    username      = models.CharField(max_length=50, unique=True)
+    email         = models.EmailField(max_length=100, unique=True)
+    phonenumber   = models.CharField(max_length=25, unique=True)
 
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
+    date_joined   = models.DateTimeField(auto_now_add=True)
+    last_login    = models.DateTimeField(auto_now_add=True)
+    is_admin      = models.BooleanField(default=False)
+    is_staff      = models.BooleanField(default=False)
+    is_active     = models.BooleanField(default=False)
     is_superadmin = models.BooleanField(default=False)
                     ###########  با ایمیل لاگین کردن  ###########
     USERNAME_FIELD = 'email'
