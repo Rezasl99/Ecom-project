b59183da9797236685dd112cbd67ed880f86cd96
Rezasl99 <Rezasoltani78@gmail.com>
2024-09-15 17:39:08 +0330

    0.0.8

--- store/models.py
+++ store/models.py
@@ -11,14 +11,14 @@ class Product(models.Model):
     product_images      = models.ImageField(upload_to='photo/products')
     product_stock       = models.IntegerField()
     is_available        = models.BooleanField(default=True)
-    category            = models.ForeignKey(Category, on_delete=models.CASCADE) #با استفاده از کاتگوری به عنوان فارن کی
+    category            = models.ForeignKey(Category, on_delete=models.CASCADE) 
     created_date        = models.DateTimeField(auto_now_add=True)
     modified_date       = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.product_name
 
-#اینم برای گرفتن لینک محصول
+# برای گرفتن لینک محصول
     def get_url(self):
         return reverse('products_detail', args=[self.category.category_slug, self.product_slug])
 
--- store/views.py
+++ store/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 
 from .models import Product
 from category.models import Category
 from carts.views import _cart_id
 from carts.models import CartItem
 from django.core.paginator import Paginator
 from itertools import chain
+from django.db.models import Q
 
 #ویو فروشگاه
 def store(request, category_slug=None):
@@ -19,7 +20,7 @@ def store(request, category_slug=None):
         product_not_available = Product.objects.all().filter(is_available=False)
     product_count = products.count() + product_not_available.count()
     all_products = list(chain(products , product_not_available))
-    paginator = Paginator(all_products, 4)
+    paginator = Paginator(all_products, 6)
     page = request.GET.get('page')
     paged_products = paginator.get_page(page)
 
@@ -44,3 +45,17 @@ def product_detail(request,category_slug, product_slug):
         'in_cart' : in_cart
     }
     return render(request, 'store/product_detail.html', context)
+
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+    products = Product.objects.none()
+    if keyword:
+        products = Product.objects.filter(Q(product_name__icontains = keyword) | Q(product_description__icontains = keyword))
+    product_count = products.count()
+    context = {
+        'product_count' : product_count,
+        'products': products
+    }
+    return render (request, 'store/store.html', context)
